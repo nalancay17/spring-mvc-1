c1be6e83f92ec070ee134323f8e7500d5cb5ebaa
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-09-05 00:23:01 -0300

    Clase Alumno y controlador para "registrarlos" usando  un objeto Model.
    El método formulario() agrega un objeto Alumno vacío como atributo del
    Model, y nos dirije a la vista del formulario.
    
    El método procesarFormulario() recibe un objeto Alumno del modelo y lo
    envía a la vista del formulario procesado usando un Model.
    
    Annotations RequestParam("parametro_request") y
    ModelAttribute("atributo_modelo"):
    
    _ Los usamos para obtener un parámetro/atributo del objeto Request/Model
    (respectivamente) y asignarlo a un parámetro en la firma de un método.
    
    _ Si se escribe un parámetro en la firma de un método y no se especifica
    de donde se obtiene, implícitamente Spring buscará entre los parámetros
    del Request alguno que haga match con el nombre del parámetro de la
    firma del método que estamos definiendo. En caso de no encontrarlo,
    buscará hacer match con algún atributo del Model.
    
    _ No usar estas annotations puede resultar más legible, pero esta
    asignación se hará de manera implícita y podría no ser claro si el dato
    se obtiene del Request o del Model.

---
+++ src/main/java/controlador/AlumnoController.java
@@ -0,0 +1,28 @@
+package controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import modelo.Alumno;
+
+@Controller
+@RequestMapping("/alumno")
+public class AlumnoController {
+
+	@GetMapping("/formulario")
+	public String formulario(Model modelo) {
+		Alumno alumno = new Alumno();
+		modelo.addAttribute("alumno", alumno);
+		return "formularioRegistroAlumno";
+	}
+
+	@PostMapping("/procesarFormulario")
+	public String procesarFormulario(@ModelAttribute("alumno") Alumno alumno, Model modelo) {
+		modelo.addAttribute("alumno", alumno);
+		return "formularioAlumnoProcesado";
+	}
+}
