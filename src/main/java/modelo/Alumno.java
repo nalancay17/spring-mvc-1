67de5aea5b25c982d29eb67e32e6407cb482bbae
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-09-05 11:55:55 -0300

    Se agrega atributo "materia" en Alumno y se usa selector en formulario.
    La materia del alumno se completa usando un selector en el formulario.
    
    NOTAR: se podrían generar las opciones y sus textos usando "th:each", por ejemplo, si disponemos de los valores de las opciones a través del Model.

--- src/main/java/modelo/Alumno.java
+++ src/main/java/modelo/Alumno.java
@@ -4,6 +4,7 @@ public class Alumno {
 
 	private String nombre;
 	private String apellido;
+	private String materia;
 
 	public String getNombre() {
 		return nombre;
@@ -19,6 +20,14 @@ public class Alumno {
 
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+
+	public String getMateria() {
+		return materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
 	}
 
 }
